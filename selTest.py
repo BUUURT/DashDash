2e64419093aac907b071e946b47e5754da73fe98
Brett Hlavka <bretthlavka@gmail.com>
2020-03-18 16:57:51 -0700

    functioning POC for master timekeeping class

--- selTest.py
+++ selTest.py
@@ -1,4 +1,7 @@
+##technical debt: add racer class to data extraction
 import time
+
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,25 +9,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-class Racer:
-    def __init__(self,name,position,laps,lastTime,bestLap,bestTime,diff,gap):
-        self.name = name
-        self.position = position
-        self.laps = laps
-        self.lastTime = lastTime
-        self.bestLap = bestLap
-        self.bestTime = bestTime
-        self.diff = diff
-        self.gap = gap
+rammerId = '#4'
 
-    def __str__(self):
-        return str(self.name,self.position)
+class RacerTable:
+    def __init__(self,df,teamId):
+        self.df = df
+        self.teamId = teamId
 
     def __repr__(self):
-        return str(self.name,self.position)
+        return self.df.head()
+
+    def __str__(self):
+        return self.df.head()
+
+    def update(self,row):
+        for key in row.keys():
+            if row[key] != self.df[self.df['number']==row['number']][key].item():
+                print(row['number'],key,row[key])
 
 
-driver = webdriver.Chrome(executable_path=r'C:/py/ChromeDriver.exe')
+
+driver = webdriver.Chrome(executable_path=r'C:/py/Rammer/ChromeDriver.exe')
 driver.get(r'http://api.race-monitor.com/Timing/?raceid=37872&source=www.race-monitor.com')
 
 timeout = 30
@@ -34,8 +39,45 @@ try:
 except TimeoutException:
     driver.quit()
 
-racerSet = set()
-timeList = driver.find_elements_by_class_name('racerRowWide')
-for racer in timeList:
-    print(racer.text)
-    # time.sleep(0.01)
+def racerParse(html_output):
+    """Converts web scraped racer data into pandas dataframe update.
+    Input of selenium mined racer row (from class racerRowWide) as raw string.
+    Output of dictionary"""
+    data = html_output.text.split('\n')
+
+
+    number = data[1].split(' ')[0]
+    name = data[1].split(' ')[1]
+
+    laps = data[data.index('Laps:')+1]
+    if laps[0].isnumeric() == False:
+        laps = None
+
+    lapTime = data[data.index('Last Time:')+1]
+    if lapTime[0].isnumeric == False:
+            lapTime = None
+
+    bestLap = data[data.index('Best Lap:')+1]
+    if bestLap[0].isnumeric == False:
+        bestLap = None
+
+    bestTime = data[data.index('Best Time:')+1]
+    if bestTime[0].isnumeric() == False:
+        bestTime = None
+
+    output = {'number':number, 'name':name, 'laps':laps, 'lapTime':lapTime, 'bestLap':bestLap, 'bestTime':bestTime}
+    return output
+
+timeRaw = driver.find_elements_by_class_name('racerRowWide')
+
+initDict = {'number':list(), 'name':list(), 'laps':list(), 'lapTime':list(), 'bestLap':list(), 'bestTime':list()}
+for i in timeRaw:
+    elm = racerParse(i)
+    for k,v in elm.items():
+        initDict[k].append(v)
+raceMaster= RacerTable(pd.DataFrame(initDict), rammerId)
+
+while True:
+    for row in timeRaw:
+        raceMaster.update(racerParse(row))
+        time.sleep(0.1)
