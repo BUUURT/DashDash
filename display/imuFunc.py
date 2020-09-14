39e2bcc5118272dc8e7b22f9ae4119d5408a4ca0
Brett Hlavka <bretthlavka@gmail.com>
2020-09-13 20:27:14 -0700

    test1

--- display/imuFunc.py
+++ display/imuFunc.py
@@ -5,14 +5,14 @@ from board import SDA, SCL
 
 class Imu:
     def __init__(self):
-        i2c=I2C(SCL,SDA)
+        i2c=I2C(SCL, SDA)
         self.imuboard = adafruit_bno055.BNO055(i2c)
     def __str__(self):
         print(self.imuboard.temperature)
 
 
-i=Imu()
-# def imu_read():
-#     i2c = I2C(SCL, SDA)
-#     imuBoard = adafruit_bno055.BNO055(i2c)
-#     print(imuBoard).temperature
+
+def imu_read():
+    i2c = I2C(SCL, SDA)
+    imuBoard = adafruit_bno055.BNO055(i2c)
+    print(imuBoard).temperature
