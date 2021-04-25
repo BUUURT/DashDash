62a29c0929e91d6cefab3e8bf7612c9bc0721958
Brett Hlavka <bretthlavka@gmail.com>
2021-04-25 16:04:01 -0700

    rpmtest fix

--- rpiFunc/rpmTest.py
+++ rpiFunc/rpmTest.py
@@ -21,15 +21,10 @@ def calculate_elapse(channel):				# callback function
 	elapse = time.monotonic() - start_timer		# elapse for every 1 complete rotation made!
 	start_timer = time.monotonic()				# let current time equals to start_timer
 
-def calculate_speed(r_cm):
+def calculate_speed():
 	global pulse,elapse,rpm
 	if elapse !=0:							# to avoid DivisionByZero error
 		rpm = 1/elapse * 60
-		# circ_cm = (2*math.pi)*r_cm			# calculate wheel circumference in CM
-		# dist_km = circ_cm/100000 			# convert cm to km
-		# km_per_sec = dist_km / elapse		# calculate KM/sec
-		# km_per_hour = km_per_sec * 3600		# calculate KM/h
-		# dist_meas = (dist_km*pulse)*1000	# measure distance traverse in meter
 		return rpm
 
 def init_interrupt():
@@ -39,7 +34,6 @@ if __name__ == '__main__':
 	init_GPIO()
 	init_interrupt()
 	while True:
-		calculate_speed(20)	# call this function with wheel radius as parameter
-		print(f'speed: {km_per_hour}')
-		# print('rpm:{0:.0f}-RPM kmh:{1:.0f}-KMH dist_meas:{2:.2f}m pulse:{3}'.format(rpm,km_per_hour,dist_meas,pulse))
+		i = calculate_speed()	# call this function with wheel radius as parameter
+		print(f'rpm: {i}')
 		sleep(0.01)
