8969a21922f4746f47a548afd21e957529ec5ab1
Brett Hlavka <bretthlavka@gmail.com>
2022-03-02 23:19:02 -0800

    turn on gpio func

--- boot_func.py
+++ boot_func.py
@@ -16,8 +16,8 @@ from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtQuick import QQuickView
 
-# from bikeClass import Bike
-# bike = Bike(_wheelspeed=False,_rpm=False,_gps=False,_imu=False,_engTemp=False)
+from bikeClass import Bike
+bike = Bike(_wheelspeed=True,_rpm=True,_gps=False,_imu=False,_engTemp=True)
 
 class Bridge(QObject):
 
